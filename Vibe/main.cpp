29bcd6c27990ec94d2112f49e549c4cc4de44227
ZhangXunfu2024051604037 <2153305605@qq.com>
2026-06-28 13:31:54 +0800

    删除不符合要求的类，使用高层类进行替代

--- Vibe/main.cpp
+++ Vibe/main.cpp
@@ -1,25 +1,35 @@
-#include <QCoreApplication>
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QIcon>  // 加这个头文件
+#include <QQmlEngine>
+#include <QQmlComponent>
+#include <QQuickWindow>
+#include <QIcon>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
         QGuiApplication app(argc, argv);
+        // 一些简单的项目名字
         QCoreApplication::setApplicationName(QStringLiteral("Vibe Media Player"));
         QCoreApplication::setApplicationVersion(QStringLiteral("0.1"));
         QCoreApplication::setOrganizationName(QStringLiteral("Vibe"));
         QCoreApplication::setOrganizationDomain(QStringLiteral("vibe.player"));
+        // 设置logo
+        app.setWindowIcon(QIcon(":/res/icons/logo_higher_res.png"));
 
-        app.setWindowIcon(QIcon(":/res/icons/logo_higher_res.png"));  // 加这一行
+        // 引擎和组件设置
+        QQmlEngine engine;
+        QQmlComponent component(&engine);
+        component.loadFromModule("Vibe", "Window");
 
-        QQmlApplicationEngine engine;
-        QObject::connect(
-                &engine,
-                &QQmlApplicationEngine::objectCreationFailed,
-                &app,
-                []() { QCoreApplication::exit(-1); },
-                Qt::QueuedConnection);
-        engine.loadFromModule("Vibe", "Window");
+        // 异常处理
+        if (component.isError()) {
+                qWarning() << component.errors();
+                return -1;
+        }
+
+        QObject *obj = component.create();
+        if (!obj) {
+                qWarning() << "Failed to create component";
+                return -1;
+        }
+
         return app.exec();
 }
